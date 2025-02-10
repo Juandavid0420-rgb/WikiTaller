0c99685240b6c738814b8370ac15df42856cf58a
juandavid0420-rgb <juandsanchez@javeriana.edu.co>
2025-02-09 19:42:23 -0500

    Subiendo Archivos Iniciales

---
+++ src/main/java/com/example/wikitaller/WikitallerApplication.java
@@ -0,0 +1,13 @@
+package com.example.wikitaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikitallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikitallerApplication.class, args);
+	}
+
+}
